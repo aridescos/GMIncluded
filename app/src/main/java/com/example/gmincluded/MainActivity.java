1cf7fcf108e2bf60611076ec1871fe341b66ef4e
aridescos <aridaifrances@gmail.com>
2021-03-19 19:30:06 +0100

    Fase prematura de actividad principal

--- app/src/main/java/com/example/gmincluded/MainActivity.java
+++ app/src/main/java/com/example/gmincluded/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.gmincluded;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtDestinatario, txtAsunto, txtContenido;
+    Button btEnviar;
+    String destinatario, asunto, contenido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btEnviar = (Button) findViewById(R.id.btnEnviar);
+        txtDestinatario = (EditText) findViewById(R.id.edtDestinatario);
+        txtAsunto = (EditText) findViewById(R.id.edtAsunto);
+        txtContenido = (EditText) findViewById(R.id.edtContenido);
+
+        btEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                destinatario = txtDestinatario.getText().toString();
+                asunto = txtAsunto.getText().toString();
+                contenido = txtContenido.getText().toString();
+
+                if (destinatario.isEmpty() || asunto.isEmpty() || contenido.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Oops, parece que falta de rellenar algún campo :)", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this, "Enviando mensaje...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
